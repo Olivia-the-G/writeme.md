8d6fc57130cb96f52a7ceea4b69cd57b49adca7d
Flesh <olivia.a.gowan@gmail.com>
2023-12-05 21:18:39 -0500

    finished code

--- Develop/demo/README.md
+++ Develop/demo/README.md
@@ -1,8 +1,8 @@
 
-# README Generator CLA [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+# t [![License: GNU AGPLv3](https://img.shields.io/badge/License-GNU_AGPLv3-orange?style=flat-square&link=https%3A%2F%2Fwww.gnu.org%2Flicenses%2Fagpl-3.0.en.html)](https://www.gnu.org/licenses/agpl-3.0.en.html)
 
 ## Description 
-This project is meant to assist developers in creating their readme files so that they can devote more time to working on their code 
+t
 
 ## Table of Contents
 
@@ -14,22 +14,22 @@ This project is meant to assist developers in creating their readme files so tha
 *[Contact](#-contact)
 
 ## Installation
-This is where you will put installation instrucitons 
+t
 
 ## Usage
-To run the application, make sure you have installed all necessary npm and then run node index.js in your terminal. Answer all of the prompts and the readme file will be created for you 
+t
 
 ## License
-This application is licensed under MIT
+This application is under the GNU AGPLv3 license. To see full license details, [click here](https://www.gnu.org/licenses/agpl-3.0.en.html)
 
 ## Contributing
-This is where you will include information on how others can contribute to your project 
+t
 
 ## Tests
-This is where any testing instructions will go
+t
 
 ## Questions
 
 If you would like to inquire about anything in this project, use the following information,
-GitHub: [Olivia-the-G](https://github.com/Olivia-the-G)
+GitHub: [t](https://github.com/t)
 Email Address: olivia.a.gowan@gmail.com
--- Develop/index.js
+++ Develop/index.js
@@ -58,8 +58,8 @@ const questions = [
       "Boost Software 1.0",
       "MIT",
       "Mozilla",
+      "NO LICENSE"
     ],
-    validate: validateInput
   },
   {
     type: 'input',
@@ -94,24 +94,6 @@ const questions = [
   }
 ];
 
-// take user input and provide url to their selected license
-function getLicense(value) {
-  if (value === "GNU AGPLv3") {
-    return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
-  } else if (value === "GNU GPLv3") {
-    return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
-  } else if (value === "GNU LGPLv3") {
-    return "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
-  } else if (value === "Apache 2.0") {
-    return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-  } else if (value === "Boost Software 1.0") {
-    return "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
-  } else if (value === "MIT") {
-    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-  } else {
-    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
-  }
-};
 
 // write README file
 function writeToFile(fileName, data) {
@@ -126,7 +108,7 @@ function writeToFile(fileName, data) {
 function init() {
   inquirer.prompt(questions).then((data) => {
     console.log(JSON.stringify(data, null, " "));
-    data.getLicense = getLicense(data.license);
+    // data.licenseSection = licenseSection(data.license);
     writeToFile("./demo/README.md", data);
   }
   )
